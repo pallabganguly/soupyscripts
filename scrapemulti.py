542884bbc6db2d10fea283c2a9bf954168032b84
Pallab <pallabkumarganguly@gmail.com>
2017-09-01 20:28:47 +0530

    modify MO

--- scrapemulti.py
+++ scrapemulti.py
@@ -4,18 +4,19 @@ from bs4 import BeautifulSoup
 # This file will get 10 Quotes current stock price from Bloomberg.
 # BSE and NSE wasnt working
 
-urls = ["https://www.bloomberg.com/quote/GOOGL:US",
-        "https://www.bloomberg.com/quote/TATA:IN",
-        "https://www.bloomberg.com/quote/TCS:IN",
-        "https://www.bloomberg.com/quote/IOCL:IN",
-        "https://www.bloomberg.com/quote/PG:IN",
-        "https://www.bloomberg.com/quote/CTSH:US",
-        "https://www.bloomberg.com/quote/STAR:MK",
-        "https://www.bloomberg.com/quote/SBIN:IN",
-        "https://www.bloomberg.com/quote/GE:US",
-        "https://www.bloomberg.com/quote/005930:KS"]
+quotes = ["GOOGL:US",
+          "TATA:IN",
+          "TCS:IN",
+          "IOCL:IN",
+          "PG:IN",
+          "CTSH:US",
+          "STAR:MK",
+          "SBIN:IN",
+          "GE:US",
+          "005930:KS"]
 
-for url in urls:
+for quote in quotes:
+    url = "https://www.bloomberg.com/quote/"+quote
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
 
